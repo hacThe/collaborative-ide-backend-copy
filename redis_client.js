34e863032809e9f7565563238022e673f5bb63f8
TungTLT <tranlethanhtung554@gmail.com>
2022-11-24 13:56:00 +0700

    remove unused code

--- redis_client.js
+++ redis_client.js
@@ -4,15 +4,6 @@ const REDIS_EVENT = require('./utils/constants')
 
 const redisClient = createClient()
 // redis event handler
-redisClient.on('error', () => console.log(redBright.bold('ERROR connecting to Redis')))
+redisClient.on(REDIS_EVENT.ERROR, () => console.log(redBright.bold('ERROR connecting to Redis')))
 
-var createRoom = async (roomID) => {
-    await redisClient.hSet(`${roomID}:info`, {
-        created: moment(),
-        updated: moment(),
-    })
-        .catch((err) => console.log(redBright.bold(`ERROR: ${err}`)))
-}
-
-module.exports.createRoomWithUser = createRoom
 module.exports.redisClient = redisClient
