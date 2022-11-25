8140d7a4793578cb1d4a69a282e9041d34d790ea
TungTLT <tranlethanhtung554@gmail.com>
2022-11-25 16:56:57 +0700

    save code API

--- routes/save-data.js
+++ routes/save-data.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 const { redisClient } = require('../redis_client')
+const { redBright } = require('chalk')
 
 /* POST save code to Redis */
 router.post('/', async (req, res) => {
@@ -8,6 +9,14 @@ router.post('/', async (req, res) => {
     const code = req.body['code']
 
     // save data to redis
+    await redisClient.hSet(`${roomId}:roomInfo`, {
+        "code": code
+    }).catch((err) => {
+        console.error(redBright.bold(`save code with ${err}`))
+        return
+    })
+
+    res.status(201).send({ "message": "Code saved!" })
 })
 
 module.exports = router
