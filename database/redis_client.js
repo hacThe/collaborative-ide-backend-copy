db2aa6bb89d434d6affddde7bb1c1f6b3b1ea293
Dương Hiển Thế <61902982+hacThe@users.noreply.github.com>
2022-12-24 18:03:10 +0700

    Update redis_client.js

--- database/redis_client.js
+++ database/redis_client.js
@@ -2,7 +2,9 @@ const { createClient } = require('redis')
 const { redBright } = require('chalk')
 const REDIS_EVENT = require('../utils/constants')
 
-const redisClient = createClient()
+const redisClient = createClient({
+    url: 'redis://red-cejaj5la4991ihma51rg:6379'
+})
 // redis event handler
 redisClient.on(REDIS_EVENT.ERROR, (e) => console.log(redBright.bold(`ERROR connecting to Redis ${e}`)))
 
